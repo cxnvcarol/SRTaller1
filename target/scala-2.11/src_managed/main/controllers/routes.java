97a08a1813e38f23961a56523281bf3929b1fb5a
Carol <cxnvcarol@gmail.com>
2015-02-19 13:01:58 -0500

    error raro arreglado

--- target/scala-2.11/src_managed/main/controllers/routes.java
+++ target/scala-2.11/src_managed/main/controllers/routes.java
@@ -9,14 +9,14 @@ public static final controllers.ReverseAssets Assets = new controllers.ReverseAs
 public static final controllers.ReverseApplication Application = new controllers.ReverseApplication();
 
 public static class javascript {
-public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();
-public static final controllers.javascript.ReverseApplication Application = new controllers.javascript.ReverseApplication();
+public static final ReverseAssets Assets = new ReverseAssets();
+public static final ReverseApplication Application = new ReverseApplication();
 }
           
 
 public static class ref {
-public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();
-public static final controllers.ref.ReverseApplication Application = new controllers.ref.ReverseApplication();
+public static final ReverseAssets Assets = new ReverseAssets();
+public static final ReverseApplication Application = new ReverseApplication();
 }
           
 }
